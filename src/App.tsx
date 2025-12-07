f8801a16645294dd017d3d93d46fd656c3bd024d
Deepak Patil <deepakpatil2612@gmail.com>
2025-12-07 16:47:06 +0530

    fix: correct title in index.html and adjust Navbar import case in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar } from "./components/navbar";
+import { Navbar } from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
